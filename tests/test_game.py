4ed258c2b5163ecc46fb771988cf16813e9c93ae
Barth <deepanfr@gmail.com>
2020-11-25 08:23:18 +0100

    test git

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,5 @@
 # tests/test_game.py
-# [...]
+# [...] - test git
     def test_unknown_word_is_invalid(self):
       new_game = Game()
       new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
